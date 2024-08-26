12550fa7a2d9c53887303dce672adc801d7e4063
Ivan Tandella <ivantandella2@gmail.com>
2024-08-26 10:30:38 +0700

    memecah komponen ke dalam file

---
+++ src/Gallery.tsx
@@ -0,0 +1,11 @@
+import Profile from "./Profile";
+export default function Gallery() {
+  return (
+    <section>
+      <h1>Para ilmuwan hebat</h1>
+      <Profile />
+      <Profile />
+      <Profile />
+    </section>
+  );
+}
