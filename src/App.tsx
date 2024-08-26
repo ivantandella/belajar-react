e8a4c9263e5d61f7854f8b0dea62b3b90ecdb2f2
Ivan Tandella <ivantandella2@gmail.com>
2024-08-26 12:00:21 +0700

    menggunakan props

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import Profile from "./Profile";
-import Gallery from "./Gallery";
+// import Profile from "./Profile";
+// import Gallery from "./Gallery";
 // import "./App.css";
 
 // export default function App() {
@@ -27,20 +27,78 @@ const person = {
   },
 };
 
-export default function TodoList() {
+// export default function TodoList() {
+//   return (
+//     <div style={person.theme}>
+//       <h1>{person.name}'s Todos</h1>
+//       <img
+//         className="avatar"
+//         src={baseUrl + person.imageId + person.imageSize + ".jpg"}
+//         alt={person.name}
+//       />
+//       <ul>
+//         <li>Improve the videophone</li>
+//         <li>Prepare aeronautics lectures</li>
+//         <li>Work on the alcohol-fuelled engine</li>
+//       </ul>
+//     </div>
+//   );
+// }
+
+// menggunakan props
+export default function Gallery() {
   return (
-    <div style={person.theme}>
-      <h1>{person.name}'s Todos</h1>
+    <div>
+      <h1>Notable Scientists</h1>
+      <Profile
+        name="Maria Skłodowska-Curie"
+        imageUrl={baseUrl + person.imageId + person.imageSize + ".jpg"}
+        imageSize={70}
+        profession="Fisikawan dan kimiawan"
+        discovery="polonium (unsur kimia)"
+        awards={[
+          "Penghargaan Nobel Fisika",
+          "Penghargaan Nobel Kimia",
+          "Medali Davy",
+          "Medali Matteucci",
+        ]}
+      />
+      <Profile
+        name="Katsuko Saruhashi"
+        imageUrl={baseUrl + person.imageId + person.imageSize + ".jpg"}
+        imageSize={70}
+        profession="Ahli Geokimia"
+        discovery="sebuah metode untuk mengukur karbon dioksida pada air laut"
+        awards={["Penghargaan Miyake Geokimia", "Penghargaan Tanaka"]}
+      />
+    </div>
+  );
+}
+
+function Profile({ name, imageUrl, imageSize, profession, discovery, awards }) {
+  return (
+    <section className="profile">
+      <h2>{name}</h2>
       <img
         className="avatar"
-        src={baseUrl + person.imageId + person.imageSize + ".jpg"}
-        alt={person.name}
+        src={imageUrl}
+        alt={name}
+        width={imageSize}
+        height={imageSize}
       />
       <ul>
-        <li>Improve the videophone</li>
-        <li>Prepare aeronautics lectures</li>
-        <li>Work on the alcohol-fuelled engine</li>
+        <li>
+          <b>Profesi: </b>
+          {profession}
+        </li>
+        <li>
+          <b>Penghargaan: {awards.length} </b>({awards.join(", ")})
+        </li>
+        <li>
+          <b>Telah Menemukan: </b>
+          {discovery}
+        </li>
       </ul>
-    </div>
+    </section>
   );
 }
